1625aa1a13f1f5384bab2c06d8bf522880e9530d
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2014-06-12 12:04:54 +0900

    イベント募集中は募集情報を表示するよう対応

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,30 @@
+$(document).ready(function() {
+  "use strict";
+  var Event = {
+    search: function() {
+      var date = new Date();
+      var defer = $.Deferred();
+      $.ajax({
+        url: "http://api.doorkeeper.jp/groups/ginzarb/events",
+          data: {since: date.toISOString()},
+          dataType: 'jsonp',
+          success: defer.resolve,
+          error: defer.reject
+      });
+      return defer.promise();
+    },
+    format: function(event) {
+      var dateStr = $.format.date(event.starts_at, "yyyy/MM/dd");
+      var text = "<h3>次回は <strong>" + dateStr + "</strong> 開催予定です。<br>お題は「" + event.title +
+        "」です。<br>申し込みは<a href=" + event.public_url + ">こちら</a>から。</h3><br>"
+      return text;
+    }
+  };
 
+  Event.search().done(function(events) {
+    if (events.length !=0) {
+      console.log(events)
+      $('.next-event').html(Event.format(events[0].event));
+    }
+  });
+});
